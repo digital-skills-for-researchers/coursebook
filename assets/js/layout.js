9afb05d3897a97e6d92316b8308fd0497bfd0df2
Tanya Gray <Tanya@wifi-staff-172-24-53-65.net.auckland.ac.nz>
2016-04-29 13:36:45 +1200

    Minor layout improvements for mobile
    Signed-off-by: Tanya Gray <Tanya@wifi-staff-172-24-53-65.net.auckland.ac.nz>

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
 
 
+
+	$('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: false // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+  );
+
+
+
+
 	// get all children of the module element
 	var moduleInfoContainer = $('.page-content');
 	var moduleChildElements = moduleInfoContainer.children();
